dce2e9d968f1f9c12809282b06b0697313153c50
innaznayda <innaznayda@gmail.com>
2019-03-17 13:49:19 +0200

    simple API

---
+++ APIRest/RestAPI/Controllers/WeatherController.cs
@@ -0,0 +1,35 @@
+﻿using RestAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RestAPI.Controllers {
+    public class WeatherController : ApiController {
+        // GET: api/Weather
+        public IEnumerable<WeatherInfo> Get() {
+            List<WeatherInfo> weatherInfoList = new List<WeatherInfo>();
+            for (int i=0; i < 10; i++) {
+                weatherInfoList.Add(new WeatherInfo {
+                    Location = $"Location {i}",
+                    Degree = i / 23 * 17,
+                    DateTime  = DateTime.Now.ToUniversalTime()
+                });              
+               
+            }
+            return weatherInfoList;
+        }
+
+        // GET: api/Weather/5
+        public WeatherInfo Get(int id) {
+           return new WeatherInfo {
+               Location = $"Location {id}",
+               Degree = id / 23 * 17,
+               DateTime = DateTime.Now.ToUniversalTime()
+           };
+
+        }
+    }
+}
---
+++ APIRest/RestAPI/Models/WeatherInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace RestAPI.Models {
+    [DataContract]
+    public class WeatherInfo {
+        [DataMember(Name ="location")]
+        public string Location { get; set; }
+        [DataMember(Name = "Degree")]
+        public float Degree { get; set; }
+        [DataMember(Name = "DateTime")]
+        public DateTime DateTime { get; set; }
+    }
+}
